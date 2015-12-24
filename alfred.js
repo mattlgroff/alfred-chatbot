54b108faf48e0547cfae4678fbfd2d10a5f5e430
Matthew Groff <mattlgroff@gmail.com>
2015-12-24 10:50:04 -0800

    Testing port changes for Heroku

--- alfred.js
+++ alfred.js
@@ -69,17 +69,26 @@ var options = {
 
 var app = express();
 
-app.get('/', function (req, res, err) {
-  //res.send('Hello World!')
-})
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-var server = app.listen(80, function () {
-  var host = server.address().address
-  var port = server.address().port
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-  console.log('Example app listening at http://%s:%s', host, port)
-})
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
 
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
 
 
 
