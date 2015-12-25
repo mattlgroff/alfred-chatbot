7ef554031e8ff665c957c2662df9825f97762386
Matthew Groff <mattlgroff@gmail.com>
2015-12-25 12:29:45 -0800

    Hopefully fixing an issue with express.

--- alfred.js
+++ alfred.js
@@ -74,7 +74,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
@@ -83,7 +83,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('index');
+    //res.render('index');
 });
 
 app.listen(port, function() {
