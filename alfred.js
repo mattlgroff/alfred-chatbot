6d2cab6a29259e5b3a1b39a349cc7e96d6959048
Matthew Groff <mattlgroff@gmail.com>
2015-12-27 17:34:01 -0800

    toString to minecrafthost

--- alfred.js
+++ alfred.js
@@ -855,7 +855,7 @@ request(options, function(err, res, body){ //Beginning of General Request
                                         //Minecraft Server Check
                                         if(message.indexOf("minecraft down") > -1 || message.indexOf("Is the minecraft server down?") > -1 || message.indexOf("minecraft down?") > -1) {
                                                 console.log(packet.d.author.username, packet.d.timestamp, packet.d.content)
-												var host = minecrafthost
+												var host = minecrafthost.toString()
 												
 												mcping(host, 25565, function(error, res) {
 												if (error){
@@ -888,9 +888,6 @@ request(options, function(err, res, body){ //Beginning of General Request
 													}
 													
 												}, 3000);												
-												
-												
-												
 												
                                         }
 										
